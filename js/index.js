4510e693b3fdc08ff122025cd553cd91acff957a
dmerinero <davidmerineroharo@gmail.com>
2018-10-05 20:16:42 +0200

    2 columns design added. Social Networks added with some styles

---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("buttonScrollUp").style.display = "block";
+  } else {
+    document.getElementById("buttonScrollUp").style.display = "none";
+  }
+}
+
+function goUp(){
+    window.scrollBy(0, -1);
+}
+
+function scrollUp() {
+    var i = 0;
+    var max = document.documentElement.scrollTop;
+
+    while(i < max) {
+        goUp(i);
+        i++;
+    }
+    
+    document.getElementById("buttonScrollUp").style.display = "none";
+}
+
+function openInNewTab(url) {
+    var win = window.open(url);
+    win.focus();
+  }
