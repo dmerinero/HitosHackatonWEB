01b585834dd47a2b11f127f0488cc6674dfcf4b9
dmerinero <davidmerineroharo@gmail.com>
2018-10-18 13:59:02 +0200

    Milestone 3 completed. Loader Codepen used for HTTP request

--- js/index.js
+++ js/index.js
@@ -17,3 +17,45 @@ function openInNewTab(url) {
   var win = window.open(url);
   win.focus();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  getStars();
+});
+
+function getStars() {
+  loadJSON('https://api.github.com/repos/dmerinero/HitosHackatonWEB',
+    function (data) { console.log(data); },
+    function (xhr) { console.error(xhr); }
+  );
+}
+
+function loadJSON(path, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    document.getElementById("starsLoader").classList.remove(["hidden"]);
+
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        if (success) {
+          var myParsed = JSON.parse(xhr.responseText);
+          showStars(myParsed.stargazers_count);
+        }
+      } else {
+        if (error) {
+          error(xhr);
+          showStars("?");
+        }
+      }
+
+      document.getElementById("starsLoader").classList.add(["hidden"]);
+    }
+  };
+
+  xhr.open("GET", path, true);
+  xhr.send();
+}
+
+function showStars(count) {
+  document.getElementById("starsNumber").innerHTML = count;
+  document.getElementById("stars").classList.remove(["hidden"]);
+}
